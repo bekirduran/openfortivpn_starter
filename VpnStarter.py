63795da5e9f52f468db02eb2d724ba16ade7cfe7
Bekir Duran <45694052+bekirduran@users.noreply.github.com>
2022-08-16 10:33:03 +0300

    Update VpnStarter.py

--- VpnStarter.py
+++ VpnStarter.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--password', type=str, help='Vpn password [MUST]')
     parser.add_argument('-a', '--auth', type=str, help='Authenticator code [MUST]',)
     parser.add_argument('-u', '--username', type=str, help='Vpn username ', default='bekir.duran')
-    parser.add_argument('-i', '--ip', type=str, help='Host ip', default='85.105.135.252')
-    parser.add_argument('-t', '--port', type=str, help='Host post', default='10443')
+    parser.add_argument('-i', '--ip', type=str, help='Host ip', default='')
+    parser.add_argument('-t', '--port', type=str, help='Host post', default='')
     parser.add_argument('-c', '--cert', type=str, help='Trusted cert', default='64d05a2b29cd5d5c1434f21a619bd582a8949a6099f53c28e5cfecd2c9aff2e8')
     args = parser.parse_args()
 
